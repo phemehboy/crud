aeaacf9e3f9ea07ac73d068738a4e32b5a1c801d
phemeh <rimater749@chodyi.com>
2023-11-14 16:44:53 -0800

    added styles and some dependencies from material ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+
+import logo from "./images/decode traders.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   console.log("Welcome");
-  return <h1>App</h1>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          CRUD
+        </Typography>
+        <img src={logo} className={classes.image} height="60" alt="logo" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
